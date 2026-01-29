d6d753a96cff67a15ca99bf4d5fea32867997742
rani-4524 <166368361+rani-4524@users.noreply.github.com>
2026-01-29 11:42:42 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,11 @@ const App = () => {
   return (
     <div>
         {/* Do not remove the main div */}
+    <PageLayout props={props}>
+          <h1>Welcome to my website</h1>
+          <p>This is the content of my website.</p>
+          <footer> &copy; 2023 My Website. All rights reserved.</footer>
+      </PageLayout>
     </div>
   )
 }
