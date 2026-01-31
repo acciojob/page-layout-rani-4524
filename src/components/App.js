90dd95aa3be9ddd4aa5454589f37ff52386a7d03
rani-4524 <166368361+rani-4524@users.noreply.github.com>
2026-01-31 15:30:46 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -6,10 +6,11 @@ const App = () => {
   return (
     <div>
         {/* Do not remove the main div */}
-    <PageLayout props={props}>
-          <h1>Welcome to my website</h1>
+    <PageLayout 
+         header ={ <h1>Welcome to my website</h1> }
+          footer = {<footer> &copy; 2023 My Website. All rights reserved.</footer>}
+    >
           <p>This is the content of my website.</p>
-          <footer> &copy; 2023 My Website. All rights reserved.</footer>
       </PageLayout>
     </div>
   )
